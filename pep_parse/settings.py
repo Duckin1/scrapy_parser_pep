c067af19eceef96924846c742133a9b1133897e3
Алмаз Миннибаев <almaz200@bk.ru>
2023-09-08 23:38:55 +0300

    pytest100%, flake8100%+csv

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -1,33 +1,26 @@
 from pathlib import Path
 
-# Пути
 BASE_DIR = Path(__file__).resolve().parent.parent
-RESULT_DIR = BASE_DIR / 'results'
+RESULT_DIR = 'results'
 
-# Бот
 BOT_NAME = 'pep_parse'
 
-# Домен
 PEP_DOMAIN = 'peps.python.org'
 ALLOWED_DOMAINS = [PEP_DOMAIN, ]
 URLS = [f'https://{PEP_DOMAIN}/']
 
-# Спайдеры
 SPIDER_MODULES = [f'{BOT_NAME}.spiders']
 
-# Правила для robots.txt
 ROBOTSTXT_OBEY = True
 
-# Настройки для записи в CSV
 FEEDS = {
-    str(RESULT_DIR / 'pep_%(time)s.csv'): {
+    f'{RESULT_DIR}/pep_%(time)s.csv': {
         'format': 'csv',
         'fields': ['number', 'name', 'status'],
         'overwrite': True,
     }
 }
 
-# Пайплайны
 ITEM_PIPELINES = {
     f'{BOT_NAME}.pipelines.PepParsePipeline': 300,
 }
