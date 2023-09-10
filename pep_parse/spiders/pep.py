19e3b6a1ca1bf8492cbdf07afea8aaae29ab4002
Алмаз Миннибаев <almaz200@bk.ru>
2023-09-10 17:34:06 +0300

    pytest100%, flake8100%+csv

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -6,7 +6,7 @@ from pep_parse.settings import ALLOWED_DOMAINS, URLS
 
 class PepSpider(scrapy.Spider):
     """
-        Пайплайн для обработки данных и создания сводной таблицы статусов PEP.
+    Пайплайн для обработки данных и создания сводной таблицы статусов PEP.
     """
     name = 'pep'
     allowed_domains = ALLOWED_DOMAINS
@@ -17,13 +17,18 @@ class PepSpider(scrapy.Spider):
             'section[id=numerical-index] tbody a::attr(href)'
         )
         for pep_link in all_pep:
-            yield response.follow(pep_link, callback=self.parse_pep)
+            # Добавляем закрывающий слеш к ссылке на страницу PEP
+            pep_url = response.urljoin(pep_link.extract())
+            yield response.follow(pep_url, callback=self.parse_pep)
 
     def parse_pep(self, response):
         name = response.css('h1.page-title::text').get().strip()
+        number = name.split('–')[0].replace('PEP', '').strip()
+        status = response.css('dt:contains("Status") + dd::text').get()
+
         data = {
             'name': name,
-            'number': name.split('–')[0].replace('PEP', '').strip(),
-            'status': response.css('dt:contains("Status") + dd::text').get()
+            'number': number,
+            'status': status,
         }
         yield PepParseItem(data)
